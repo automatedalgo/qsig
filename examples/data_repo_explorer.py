0a365624c6f6f73221f5fa3a6973644ba79955cc
automatedalgo <sam@automatedalgo.com>
2025-06-16 07:09:56 +0100

    minor comments

--- examples/data_repo_explorer.py
+++ examples/data_repo_explorer.py
@@ -2,8 +2,12 @@ import getpass
 
 import qsig
 
-# Basic example of using the DataRepo to example libraries and items
+# ------------------------------------------------------------------------------
+# Basic example of using the DataRepo to list libraries and items within those
+# libraries.
+# ------------------------------------------------------------------------------
 
+# Create a repo backed by a local filesystem.
 repo = qsig.DataRepo(f"/home/{getpass.getuser()}/DATAREPO")
 
 libs = repo.list_libraries()
